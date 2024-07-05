4e88e881eae5498c450296f50ac43a6c0adca9bc
luofm <luofm@lamda.nju.edu.cn>
2024-07-05 16:07:01 +0800

    remove dependencies

--- setup.py
+++ setup.py
@@ -8,15 +8,5 @@ setup(
     description="An implementation of RNN policy and value function in off-policy RL",
     packages=find_packages(),
     install_requires=[
-        # "gym", # gym==0.15.4 for rmdp and rl_generalization tasks
-        # "smart-logger @ git+https://github.com/FanmingL/SmartLogger.git",
-        # "torch",
-        # "tqdm",
-        # "numpy",
-        # "pybullet==3.2.5",
-        # "pycolab==1.2.0",
-        # 'roboschool',
-        # "box2d-py",
-        # "Box2D"
     ],
 )
